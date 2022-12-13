ee83cb5dcf2ee169d5c30f093e887295d0cdca70
Lee <94841127+galaxian@users.noreply.github.com>
2022-12-13 20:54:16 +0900

    백준 수 찾기 문제 이분탐색 풀이

--- baekjoon/binary search/수 찾기.py
+++ baekjoon/binary search/수 찾기.py
@@ -1,3 +1,26 @@
+# 문제
+# N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+#
+# 입력
+# 첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다.
+# 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다.
+# 다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다. 다음 줄에는 M개의 수들이 주어지는데,
+# 이 수들이 A안에 존재하는지 알아내면 된다.
+# 모든 정수의 범위는 -231 보다 크거나 같고 231보다 작다.
+#
+# 출력
+# M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
+
+# 이분 탐색의 기초적인 문제로 이를 이용한 풀이를 하였다.
+# 1. 리스트 A를 정렬
+# 2. 시작 idx를 min, 마지막 idx를 max로 설정
+# 3. min, max를 사용해 중간 idx mid 계산
+# 4. A의 mid 값과 target을 비교
+#   -1 a[mid] == target 일 경우 1 출력
+#   -2 a[mid] => target 일 경우 mid 보다 큰 범위에서 탐색
+#   -3 a[mid] < target 일 경우 mid 보다 작은 범위에서 탐색
+# 5. A[min] > A[max] 경우 -1 출력
+
 N = int(input())
 A = list(map(int, input().split()))
 M = int(input())
@@ -13,13 +36,13 @@ for i in range(M):
 
     target = arr[i]
 
-    while min <= max:
-        if A[mid] == target:
+    while min <= max:  # idx가 역전되지 않는 경우 동작
+        if A[mid] == target:  # mid값이 target인 경우
             print(1)
             break
-        if A[mid] < target:
+        if A[mid] < target:  # mid가 target보다 작은 경우 min값 변경
             min = mid + 1
-        else:
+        else:  # mid가 target보다 작은 경우 max값 변경
             max = mid - 1
         mid = (min + max) // 2
     if min > max:
