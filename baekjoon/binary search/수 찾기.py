016f2aa2bb2238b9e9d39ccb62bd625037f6c4b6
Lee <94841127+galaxian@users.noreply.github.com>
2022-12-15 19:36:38 +0900

    백준 수 찾기 set 사용한 풀이 추가

--- baekjoon/binary search/수 찾기.py
+++ baekjoon/binary search/수 찾기.py
@@ -11,39 +11,59 @@
 # 출력
 # M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
 
-# 이분 탐색의 기초적인 문제로 이를 이용한 풀이를 하였다.
-# 1. 리스트 A를 정렬
-# 2. 시작 idx를 min, 마지막 idx를 max로 설정
-# 3. min, max를 사용해 중간 idx mid 계산
-# 4. A의 mid 값과 target을 비교
-#   -1 a[mid] == target 일 경우 1 출력
-#   -2 a[mid] => target 일 경우 mid 보다 큰 범위에서 탐색
-#   -3 a[mid] < target 일 경우 mid 보다 작은 범위에서 탐색
-# 5. A[min] > A[max] 경우 -1 출력
+def use_binary_search():
+    # 이분 탐색의 기초적인 문제로 이를 이용한 풀이를 하였다.
+    # 1. 리스트 A를 정렬
+    # 2. 시작 idx를 min, 마지막 idx를 max로 설정
+    # 3. min, max를 사용해 중간 idx mid 계산
+    # 4. A의 mid 값과 target을 비교
+    #   -1 a[mid] == target 일 경우 1 출력
+    #   -2 a[mid] => target 일 경우 mid 보다 큰 범위에서 탐색
+    #   -3 a[mid] < target 일 경우 mid 보다 작은 범위에서 탐색
+    # 5. A[min] > A[max] 경우 -1 출력
 
-N = int(input())
-A = list(map(int, input().split()))
-M = int(input())
-arr = list(map(int, input().split()))
+    N = int(input())
+    A = list(map(int, input().split()))
+    M = int(input())
+    arr = list(map(int, input().split()))
 
-A.sort()
+    A.sort()
 
-for i in range(M):
-
-    min = 0
-    max = N - 1
-    mid = (min + max) // 2
-
-    target = arr[i]
-
-    while min <= max:  # idx가 역전되지 않는 경우 동작
-        if A[mid] == target:  # mid값이 target인 경우
-            print(1)
-            break
-        if A[mid] < target:  # mid가 target보다 작은 경우 min값 변경
-            min = mid + 1
-        else:  # mid가 target보다 작은 경우 max값 변경
-            max = mid - 1
+    for i in range(M):
+        min = 0
+        max = N - 1
         mid = (min + max) // 2
-    if min > max:
-        print(0)
+
+        target = arr[i]
+
+        while min <= max:  # idx가 역전되지 않는 경우 동작
+            if A[mid] == target:  # mid값이 target인 경우
+                print(1)
+                break
+            if A[mid] < target:  # mid가 target보다 작은 경우 min값 변경
+                min = mid + 1
+            else:  # mid가 target보다 작은 경우 max값 변경
+                max = mid - 1
+            mid = (min + max) // 2
+        if min > max:
+            print(0)
+
+
+def use_set():
+    # set의 시간 복잡도가 O(1)이라는 점을 착안하여
+    # 이분탐색 대신 set을 사용해 문제를 해결해 보았다
+
+    N = int(input())
+    A = set(map(int, input().split()))  # input을 set으로 받았다.
+    M = int(input())
+    arr = list(map(int, input().split()))
+
+    for i in arr:
+        if i in A:  # set을 탐색하여 속도 향상
+            print(1)
+        else:
+            print(0)
+
+
+use_binary_search()
+use_set()
