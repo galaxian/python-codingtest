a6a507719262080dd749c9473766d67cdc031728
Lee <94841127+galaxian@users.noreply.github.com>
2022-12-22 21:09:42 +0900

    백준 수 정렬하기 문제 풀이

--- baekjoon/sorting/위에서 아래로.py
+++ baekjoon/sorting/위에서 아래로.py
@@ -1,9 +1,15 @@
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+
+# 간단한 정렬 문제로 sorted를 사용해 오름차순 결과를 출력했다.
+
 n = int(input())
 
 array = []
 for i in range(n):
     array.append(int(input()))
 
+# array를 오름차순 정렬
 array = sorted(array)
 
 for i in array:
