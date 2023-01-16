647d4b46167854078ccc7b9f5bdcea31dc1a4f4f
Lee <94841127+galaxian@users.noreply.github.com>
2023-01-16 14:41:59 +0900

    바닥 공사 top down 방식 추가 풀이

--- codingtest/dynamic programming/바닥공사.py
+++ codingtest/dynamic programming/바닥공사.py
@@ -1,12 +1,31 @@
 n = int(input())
 
-dp = [0] * (n + 1)
 
-dp[1] = 1
-dp[2] = 3
-dp[3] = 5
+def bottom_up(x):
+    dp = [0] * (n + 1)
 
-for i in range(4, n + 1):
-    dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 796796
+    dp[1] = 1
+    dp[2] = 3
+    dp[3] = 5
 
-print(dp[n])
+    for i in range(4, n + 1):
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 796796
+    return dp[x]
+
+
+def top_down(x):
+    dp = [0] * (n + 1)
+
+    dp[1] = 1
+    dp[2] = 3
+    dp[3] = 5
+
+    if dp[x] != 0:
+        return dp[x]
+
+    dp[x] = top_down(x - 1) + (top_down(x - 2) * 2) % 796796
+    return dp[x]
+
+
+print(bottom_up(n))
+print(top_down(n))
