2a955bba4008e849fc9f04dec7fe60648ceea788
Lee <94841127+galaxian@users.noreply.github.com>
2023-01-18 22:05:09 +0900

    linked list append 로직 구현

--- 자료구조/linkedlist.py
+++ 자료구조/linkedlist.py
@@ -8,3 +8,10 @@ class LinkedList:
     def __init__(self, data):
         self.head = Node(data)
 
+    def append(self, data):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(data)
+
+
