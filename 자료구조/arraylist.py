b0be9393d0d5f220a57ff7cebfd03e15560a47a8
Lee <94841127+galaxian@users.noreply.github.com>
2023-01-26 02:10:51 +0900

    arraylist index 조회 기능 구현

--- 자료구조/arraylist.py
+++ 자료구조/arraylist.py
@@ -6,3 +6,8 @@ class ArrayList:
     def append(self, data):
         self.list.append(data)
         self.cnt += 1
+
+    def get(self, idx):
+        if idx < 0 or idx >= self.cnt:
+            print("유효한 인덱스가 아닙니다.")
+        print(self.list[idx])
