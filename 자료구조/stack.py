3d93938b0f0d8d722bd1108df7c092fe9043d02c
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-02 02:02:33 +0900

    스택 value를 push 메서드 구현

---
+++ 자료구조/stack.py
@@ -0,0 +1,11 @@
+from 자료구조.linkedlist import Node
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, value):
+        new_head = Node(value)
+        new_head.next = self.head
+        self.head = new_head
