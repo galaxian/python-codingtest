1e9bc8b6d23b2854d496e9d7e089a3024a11744e
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-09 17:55:02 +0900

    큐가 비어있는지 확인하는 메서드 구현

--- 자료구조/queue.py
+++ 자료구조/queue.py
@@ -4,3 +4,11 @@ class Node:
         self.next = None
 
 
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def is_empty(self):
+        return self.head is None
+
