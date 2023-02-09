31230487df2c565b29d9a00587e4f0c9b3cb1cb4
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-09 17:56:58 +0900

    큐에 데이터를 삽입하는 enqueue 기능 구현

--- 자료구조/queue.py
+++ 자료구조/queue.py
@@ -9,6 +9,11 @@ class Queue:
         self.head = None
         self.tail = None
 
+    def enqueue(self, value):
+        new_node = Node(value)
+        self.head.next = new_node
+        self.tail = new_node
+
     def is_empty(self):
         return self.head is None
 
