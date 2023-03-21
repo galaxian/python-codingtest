4916c241af86596ef2e6b8b9785ec07f04858d17
Lee <94841127+galaxian@users.noreply.github.com>
2023-03-21 22:05:16 +0900

    럭키 스트레이트 코드

--- codingtest/implemetation/럭키 스트레이트.py
+++ codingtest/implemetation/럭키 스트레이트.py
@@ -3,15 +3,15 @@ n = int(input())
 nums = str(n)
 length = len(nums)
 
-left = 0
+total = 0
+
 for i in range(0, length//2):
-    left += int(nums[i])
+    total += int(nums[i])
 
-right = 0
 for i in range(length//2, length):
-    right += int(nums[i])
+    total -= int(nums[i])
 
-if left == right:
+if total == 0:
     print('LUCKY')
 else:
     print('READY')
