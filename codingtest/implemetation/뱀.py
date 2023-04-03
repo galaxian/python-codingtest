0edf89334346e3c87b93fc6526de1cc2df58ea3e
Lee <94841127+galaxian@users.noreply.github.com>
2023-04-03 15:14:30 +0900

    뱀 리팩토링

--- codingtest/implemetation/뱀.py
+++ codingtest/implemetation/뱀.py
@@ -15,34 +15,41 @@ for _ in range(l):
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-x, y = 1, 1
-arr[x][y] = 2
-direction = 0
-time = 0
-idx = 0
-q = [(x, y)]
-while True:
-    nx = x + dx[direction]
-    ny = y + dy[direction]
-    if 1 <= nx <= n and 1 <= ny <= n and arr[nx][ny] != 2:
-        if arr[nx][ny] == 0:
-            arr[nx][ny] = 2
-            q.append((nx, ny))
-            px, py = q.pop(0)
-            arr[px][py] = 0
-        if arr[nx][ny] == 1:
-            arr[nx][ny] = 2
-            q.append((nx, ny))
-    else:
-        time += 1
-        break
-    x, y = nx, ny
-    time += 1
-    if idx < l and time == rotate[idx][0]:
-        if rotate[idx][1] == "L":
-            direction = (direction - 1) % 4
-        else:
-            direction = (direction + 1) % 4
-        idx += 1
 
-print(time)
+def turn(direction, cor):
+    if cor == "L":
+        return (direction - 1) % 4
+    return (direction + 1) % 4
+
+
+def play():
+    x, y = 1, 1
+    arr[x][y] = 2
+    direction = 0
+    time = 0
+    idx = 0
+    q = [(x, y)]
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if 1 <= nx <= n and 1 <= ny <= n and arr[nx][ny] != 2:
+            if arr[nx][ny] == 0:
+                arr[nx][ny] = 2
+                q.append((nx, ny))
+                px, py = q.pop(0)
+                arr[px][py] = 0
+            if arr[nx][ny] == 1:
+                arr[nx][ny] = 2
+                q.append((nx, ny))
+        else:
+            time += 1
+            break
+        x, y = nx, ny
+        time += 1
+        if idx < l and time == rotate[idx][0]:
+            direction = turn(direction, rotate[idx][1])
+            idx += 1
+    return time
+
+
+print(play())
