3661168a5ee9734b5a369efa6f1fadf78376ae52
Lee <94841127+galaxian@users.noreply.github.com>
2023-04-12 17:39:54 +0900

    feat(블랙잭) : 백준 블랙잭 combination 사용 코드 추가

--- codingtest/brute force/백준 블랙잭.py
+++ codingtest/brute force/백준 블랙잭.py
@@ -1,15 +1,22 @@
+from itertools import combinations
+
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
 
 result = 0
 
-for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
-            temp = cards[i] + cards[j] + cards[k]
-            if temp > m:
-                continue
-            else:
-                result = max(result, temp)
+# for i in range(n):
+#     for j in range(i+1, n):
+#         for k in range(j+1, n):
+#             temp = cards[i] + cards[j] + cards[k]
+#             if temp > m:
+#                 continue
+#             else:
+#                 result = max(result, temp)
+
+for cards in combinations(cards, 3):
+    temp = sum(cards)
+    if result < temp <= m:
+        result = temp
 
 print(result)
