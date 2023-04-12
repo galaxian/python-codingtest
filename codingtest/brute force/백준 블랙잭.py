90515b7bd0bc62fcf6afa43ac44789d49f043773
Lee <94841127+galaxian@users.noreply.github.com>
2023-04-12 17:31:40 +0900

    feat(블랙잭) : 백준 블랙잭 코드

---
+++ codingtest/brute force/백준 블랙잭.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+
+result = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            temp = cards[i] + cards[j] + cards[k]
+            if result < temp <= m:
+                result = temp
+
+print(result)
