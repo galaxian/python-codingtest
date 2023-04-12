2377d7857bf8e651753de3978a7da258ebaa518c
Lee <94841127+galaxian@users.noreply.github.com>
2023-04-12 18:00:11 +0900

    ref(알파벳 찾기) : 백준 알파벳 찾기 result 리스트 삭제 및 반복문에서 print 하도록 수정

--- codingtest/implemetation/백준 알파벳 찾기.py
+++ codingtest/implemetation/백준 알파벳 찾기.py
@@ -3,9 +3,5 @@ from string import ascii_lowercase
 s = str(input())
 alphabet_list = list(ascii_lowercase)
 
-result = []
-
 for alphabet in alphabet_list:
-    result.append(str(s.find(alphabet)))
-
-print(' '.join(result))
+    print(s.find(alphabet), end=' ')
