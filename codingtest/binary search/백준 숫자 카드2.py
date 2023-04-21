333f17e8b11b43f31b94333693fdf45320651f3d
Lee <94841127+galaxian@users.noreply.github.com>
2023-04-21 23:01:03 +0900

    feat(숫자 카드2) : 백준 숫자 카드2 bisect 사용 코드

--- codingtest/binary search/백준 숫자 카드2.py
+++ codingtest/binary search/백준 숫자 카드2.py
@@ -1,3 +1,5 @@
+from bisect import bisect_left,bisect_right
+
 n = int(input())
 cards_num = list(map(int, input().split()))
 m = int(input())
@@ -6,36 +8,7 @@ find_num = list(map(int, input().split()))
 cards_num.sort()
 result = []
 
-
-def lower_bound(arr, target):
-    cur_min = 0
-    cur_max = len(arr)
-
-    while cur_min < cur_max:
-        mid = (cur_min + cur_max) // 2
-        if target <= arr[mid]:
-            cur_max = mid
-        else:
-            cur_min = mid + 1
-
-    return cur_min
-
-
-def upper_bound(arr, target):
-    cur_min = 0
-    cur_max = len(arr)
-
-    while cur_min < cur_max:
-        mid = (cur_min + cur_max) // 2
-        if target < arr[mid]:
-            cur_max = mid
-        else:
-            cur_min = mid + 1
-
-    return cur_min
-
-
 for i in range(m):
-    result.append(upper_bound(cards_num, find_num[i]) - lower_bound(cards_num, find_num[i]))
+    result.append(bisect_right(cards_num, find_num[i]) - bisect_left(cards_num, find_num[i]))
 
 print(*result)
